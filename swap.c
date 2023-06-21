488eb7cf6cafc079c4b9f9203dc42437dd8842a1
MelSaina <gabbysiana@gmail.com>
2023-06-20 23:03:08 -0700

    3

--- swap.c
+++ swap.c
@@ -5,7 +5,7 @@ void swap(stack_t **stack, unsigned int line_number)
 
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		printf("L%u: can't swap, stack too short\n", line_number);
+		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	(*stack)->n = (*stack)->next->n;

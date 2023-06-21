e7081a9bb40c91366a62366d8d8d20fefa7aee91
MelSaina <gabbysiana@gmail.com>
2023-06-21 10:52:32 -0700

    try

--- mod.c
+++ mod.c
@@ -1,11 +1,13 @@
+#include "monty.h"
+
 void mod_op(stack_t **stack, unsigned int line_number)
 {
+	stack_t *top = *stack;
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%u: can't mod, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	stack_t *top = *stack;
 	if (top->n == 0)
 	{
 		fprintf(stderr, "L%u: division by zero\n", line_number);
